72b44350976d9426ab525b163dba0f977779412e
NithishKumar-coder <54560557+NithishKumar-coder@users.noreply.github.com>
2019-11-20 10:38:09 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -3,36 +3,37 @@ def mains():
   print("name:",name)
   print('phone:',ph)
   print('email:',email)
-
 name=input()
 ph=input()
 email=input()
 distance=int(input())
-
+def qwr():
+  print('is price affordable')
+  a=input()
+  if a=='yes':
+    print('thanks') 
+    mains() 
+  else:
+    fun()
+    mains()
 def fun():
     mot=input()
     if mot=='car':
         fair=distance*30
         print(fair)
+        qwr()
         return fair
     elif mot=='auto':
         fair=distance*20
         print(fair)
+        qwr()
         return(fair)
     elif mot=='bike':
         fair=distance*10
         print(fair)
+        qwr()
         return(fair)
+        qwr()
     else:
-        return'not available'
-    
+        print('not available')
 fun()
-print('is price affordable')
-a=input()
-
-if a=='yes':
-    print('thanks') 
-    mains() 
-else:
-    fun()
-    mains()
