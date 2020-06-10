f36309f9f76fb9e07d5d8223a529be1d1b639290
Nithish Kumar <54560557+NithishKumar-coder@users.noreply.github.com>
2020-06-10 14:41:37 +0530

    Delete pattern.py

--- pattern.py
+++
@@ -1,8 +0,0 @@
-for i in range (0,6):
-    for j in range(0, i + 1):
-        print("*", end=' ')
-    print("\r")
-for i in range (6, 0, -1):
-    for j in range(0, i -2):
-        print("*", end=' ')
-    print("\r")
