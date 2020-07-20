b2a669d3f7d480a47ef65fdaf32d3487558113af
Nithish Kumar <54560557+NithishKumar-coder@users.noreply.github.com>
2020-07-20 21:14:48 +0530

    Update lead.py

--- leadgenerator/lead.py
+++ leadgenerator/lead.py
@@ -1,11 +1,11 @@
 import urllib.request
-from newspaper import Article
 import re
 import logging
 import json
 import csv
 import help
-import usaddress
+from help import helper
+from about_details import about_list
 from bs4 import BeautifulSoup
 
 #for extracting webpage
@@ -23,18 +23,7 @@ def get_webpage(url):
 #for getting text from webpage
 def get_webpage_text(html):
     soup=BeautifulSoup(html,'lxml')
-    content_text=soup.text
-    '''def visible(element):
-            #including elements in style,script,head 
-        if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
-            return False
-            #for returning texts
-        elif re.match('<!--.*-->', str(element.encode('utf-8'))):
-            return False
-        return True
-    result =list(filter(visible, text)) #The filter() function returns  text were the items are filtered through visible() function to test if the item is accepted or not.
-        #replacing all \n characters obtained in result
-    content_text=("".join(result)).replace("\n"," ")  '''     
+    content_text=soup.text  
     return content_text
 
 #for getting list of companies and their contact links
@@ -56,36 +45,32 @@ def get_contact_page_link(html):
     soup = BeautifulSoup(html, 'lxml')
     for tag in soup.find_all('a'):
         try:
-            link_ab=tag.attrs['href']
-            name=tag.text
-            title=["Contact","Offices","about","contact","support"]
-            for item in title:
-                if item in name:
-                    rock.append(link_ab)
+            if tag.has_attr('href'):
+                link_ab=tag.attrs['href']           
+                title=["Offices","about","contact","locations"]
+                for item in title:
+                    if item in link_ab:
+                        rock.append(link_ab)
         except:
             link_ab=tag.get('href')
-            name=tag.text
-            title=["Contact","Offices","about","contact","support"]
-            for item in title:
-                if item in name:
-                    rock.append(link_ab)
+            if len(link_ab)!=0:
+                title=["Offices","about","contact","locations"]
+                for item in title:
+                    if item in link_ab:
+                        rock.append(link_ab)
     rock=list(dict.fromkeys(rock))
     return rock
 
 #for getting us location
 def get_location(text):
-    local_list=[]
-    address=help.helper(text)
-    for i in address:
-        if i not in local_list:
-            local_list.append(i)
-    return local_list
+    rockers=help.helper(text)
+    return rockers
 
 #saaving as a json
 def save_to_json(filename,json_dict):
      with open(filename, "w") as f:
-            f.write(json.dumps(json_dict, sort_keys=False, indent=2, separators=(',', ': ')))
-
+        f.write(json.dumps(json_dict, sort_keys=False, indent=2, separators=(',', ': ')))
+     return None
 #coverting to csv
 def json_to_csv_file(json_filename,csv_filename):
     with open(json_filename) as json_file: 
@@ -95,9 +80,10 @@ def json_to_csv_file(json_filename,csv_filename):
             temp.append({"company name":i,"location":data[i]})
         fields = ["company name","location"]  
         with open(csv_filename, 'w') as csvfile: 
-            writer = csv.DictWriter(csvfile, fieldnames = fields)  
+            writer = csv.DictWriter(csvfile, fieldnames = fields, delimiter=',')  
             writer.writeheader()  
             writer.writerows(temp) 
+    return None
 
 
 if __name__=='__main__':
@@ -115,39 +101,42 @@ if __name__=='__main__':
     html= get_webpage(url)
     compa_list=get_list(html)
     print(compa_list)
-
+    print("\n")
+    print("number of companies whose address we need to find is:",len(compa_list))
     address=[]
     for company in compa_list:
         complete=[]
         url=company[1]
         page_html=get_webpage(url)
-        try:
-            if page_html==None:
-                print("can't access contact page of:"+company[0]+'\n')
-                pass
+        if page_html==None:
+            print("can't access contact page of:"+company[0]+'\n')
+            invalid_html.append(company[1])
+            pass
+        else:
+            contact_list=get_contact_page_link(page_html)
+            if len(contact_list):
+                for i in contact_list:
+                    if i.startswith('/'):
+                        complete.append(company[1]+i)
+                    else:
+                        complete.append(i)
+                address.append([company[0],complete])
             else:
-                contact_list=get_contact_page_link(page_html)
-                if len(contact_list):
-                    for i in contact_list:
-                        if i.startswith('/'):
-                            complete.append(company[1]+i)
-                        else:
-                            complete.append(i)
-                    address.append([company[0],complete])
-                else:
-                    print("no location details available for"+company[0]+'\n')
-                    logger.setLevel(logging.DEBUG)
-                    logger.info("Sorry! no details are available")
-                    logger.info(company[0]) 
-        except:
-            invalid_html.append(company[0])
+                print("no location details available for"+company[0]+'\n')
+                logger.setLevel(logging.DEBUG)
+                logger.info("Sorry! no details are available")
+                logger.info(company[0]) 
 
+    print("contact links and theeir respective companies\n\n")
+    print(complete)
+    print("companies with some issues:\n")
+    print("\t",invalid_html)
     empty=[]        
     filename="new.json"
     com_dict={}
-    for company in address:
+    for company in about_list:
         name=company[0]
-        url=company[1][0]
+        url=company[1]
         urll=get_webpage(url)
         
         if urll==None:
@@ -159,19 +148,12 @@ if __name__=='__main__':
             if len(loction_list):
                 com_dict.update({name:loction_list})
                 print("Company_Name:",name)
-                print("Addressess:",loction_list)
+                print("Address list:\n")
+                print(loction_list)
                 print('\n')
-            else:
-                empty.append(name)
     
     print("\n companies and their respective addressess\n")
     print(com_dict)
-    print("\n companies with no address")
-    print(empty)
-    for i in empty:
-        logger.setLevel(logging.DEBUG)
-        logger.info(i)
-    print(invalid_html)
 
     save_to_json(filename,com_dict)
     json_to_csv_file(filename,"new.csv")
